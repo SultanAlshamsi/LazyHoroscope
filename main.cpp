3ddfd31274e000c7528df17bea52a5dc8820eb72
ssalshamsi <ssalshamsi@mail.csuchico.edu>
2018-02-13 12:22:47 -0800

    lucky number

--- main.cpp
+++ main.cpp
@@ -7,10 +7,30 @@ using namespace std;
 int main()
 {
   //declare variables
+    string firstname;
+    string lastname;
+
 
   //get user input
 
+  cout<<"What is your first name?\n";
+  cin>>firstname;
+
+cout<<"What is your last name?\n";
+cin>>lastname;
+
   //tell fortune
+    cout<<"Welcome,"<<firstname[0]<<"."<<lastname[0]<<".,here is your fortune...\n";
+    
+
+    //predict lucky number
+
+    int lucky;
+    lucky = firstname.length();
+
+    cout<<"your lucky number is "<<lucky<<endl;
+
+
 
   return 0;
 }
