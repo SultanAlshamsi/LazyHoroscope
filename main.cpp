4f192b611c961875a7d735b6077b23a1ae7b08eb
ssalshamsi <ssalshamsi@mail.csuchico.edu>
2018-02-15 23:03:13 -0800

    Lazy

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Sultan Alshamsi 
 #include<iostream>
 #include<string>
 
@@ -7,30 +7,47 @@ using namespace std;
 int main()
 {
   //declare variables
-    string firstname;
-    string lastname;
+  string first;
+
+  string last;
+
+  int lucky;
+            
+            
+            
+            //get user input
+   cout<<"What is your first name?"<<endl;
+                    
+                    cin>>first;
+
+       cout<<"What is your last name?"<<endl;
+                     
+                     cin>>last;
+                                  //tell fortune
+                  cout<<"Welcome, " << first[0] << "." << last[0] << "." << ", here is your fortune..." <<endl;
+                                  cout<< "your lucky number is " << first.length() <<endl;
+     if ( first[0]== 'a' || first[0]=='e' || first[0]== 'i' || first[0]== 'o' || first[0]== 'u' || first[0]== 'A' || first[0]== 'E' || first[0]== 'I' || first[0]== 'O' || first[0]== 'U')
+                                                             {
+                                                 cout<<"you are destined to be famous!\n";
+                                                                                }
+               else if ( first[0]>65 && first[0]<90 || first[0]> 97 && first[0]<122 )
+
+                       {cout<< "you should keep a low profile.\n";
+                                                                                                        }
 
 
-  //get user input
+                                 int len=last.length();
+         if ( last[len-1] == 'a' || last[len-1] == 'e' || last[len-1] == 'i' || last[len-1] == 'o' || last[len-1] == 'u')
 
-  cout<<"What is your first name?\n";
-  cin>>firstname;
-
-cout<<"What is your last name?\n";
-cin>>lastname;
-
-  //tell fortune
-    cout<<"Welcome,"<<firstname[0]<<"."<<lastname[0]<<".,here is your fortune...\n";
-    
-
-    //predict lucky number
-
-    int lucky;
-    lucky = firstname.length();
-
-    cout<<"your lucky number is "<<lucky<<endl;
+                                                                                                                        {
+                                                                                                                                            cout<< "you have already met your true love.\n";
+                                                                                                                                                                  cout<< "have a good day!\n";
+                                                                                                                                                                                          }
 
 
 
-  return 0;
-}
+                                                                                                                                                                                                                    else{ cout<<"have a good day!\n";
+                                                                                                                                                                                                                                                  }
+
+                                                                                                                                                                                                                                                                                  return 0;
+                                                                                                                                                                                                                                                                                                      }
